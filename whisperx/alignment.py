09d1f514dcb44507bad754ac0f4d136765e16bfd
Shivin Mangal <94225434+ShivinM-17@users.noreply.github.com>
2023-10-16 16:27:24 +0530

    Update alignment.py
    Experimenting with word-level alignment

--- whisperx/alignment.py
+++ whisperx/alignment.py
@@ -312,6 +312,8 @@ def align(
 
                 word_segment['token'] =  nltk.pos_tag([word_text])[0][1]
 
+                print(word_segment)
+
                 sentence_words.append(word_segment)
             
             aligned_subsegments.append({
@@ -345,6 +347,8 @@ def align(
     word_segments: List[SingleWordSegment] = []
     for segment in aligned_segments:
         word_segments += segment["words"]
+        print(segment["words"])
+        print()
 
     return {"segments": aligned_segments, "word_segments": word_segments}
 
